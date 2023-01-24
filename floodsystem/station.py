1819237fddd8ef31c6e9f47de3056b29e563db37
Josh Richmond <jtrichmond222@gmail.com>
2023-01-24 14:48:02 +0000

    Changed station class attributes to properties

--- floodsystem/station.py
+++ floodsystem/station.py
@@ -9,25 +9,27 @@ for manipulating/modifying station data
 
 class MonitoringStation:
     """This class represents a river level monitoring station"""
+    #Changed to add property decorators for extension task. Effectively read only, unless setter methods added
+    #Variable names now have leading underscores to indicate protected status
 
     def __init__(self, station_id, measure_id, label, coord, typical_range,
                  river, town):
 
-        self.station_id = station_id
-        self.measure_id = measure_id
+        self._station_id = station_id
+        self._measure_id = measure_id
 
         # Handle case of erroneous data where data system returns
         # '[label, label]' rather than 'label'
-        self.name = label
+        self._name = label
         if isinstance(label, list):
-            self.name = label[0]
+            self._name = label[0]
 
-        self.coord = coord
-        self.typical_range = typical_range
-        self.river = river
-        self.town = town
+        self._coord = coord
+        self._typical_range = typical_range
+        self._river = river
+        self._town = town
 
-        self.latest_level = None
+        self._latest_level = None
 
     def __repr__(self):
         d = "Station name:     {}\n".format(self.name)
@@ -39,33 +41,40 @@ class MonitoringStation:
         d += "   typical range: {}".format(self.typical_range)
         return d
 
-    def get_station_id(self) -> str:
+    @property
+    def station_id(self) -> str:
         """returns station id"""
-        return self.station_id
+        return self._station_id
 
-    def get_measure_id(self) -> str:
+    @property
+    def measure_id(self) -> str:
         """returns measure id of station"""
-        return self.measure_id
+        return self._measure_id
 
-    def get_name(self) -> str:
+    @property
+    def name(self) -> str:
         """returns name of station"""
-        return self.name
+        return self._name
 
-    def get_coord(self) -> tuple[float, float]:
+    @property
+    def coord(self) -> tuple[float, float]:
         """returns the station's coordinates"""
-        return self.coord
+        return self._coord
 
-    def get_typical_range(self) -> tuple[float, float]:
+    @property
+    def typical_range(self) -> tuple[float, float]:
         """returns the typical water level range of the station"""
-        return self.typical_range
+        return self._typical_range
 
-    def get_river(self) -> str:
+    @property
+    def river(self) -> str:
         """Returns the river on which the station is located"""
-        return self.river
+        return self._river
 
-    def get_town(self) -> str:
+    @property
+    def town(self) -> str:
         """returns the closest town to the station"""
-        return self.town
+        return self._town
 
     
 
