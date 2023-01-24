604dd9db061b02e7529d8b658a2865549d79c578
Josh Richmond <jtrichmond222@gmail.com>
2023-01-24 15:32:58 +0000

    Changed distance module to use haversine. Error when running geo.py with relative install

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -9,34 +9,23 @@ geographical data.
 from .utils import sorted_by_key  # noqa
 from .station import MonitoringStation
 from math import sqrt # for distances
+from haversine import haversine # need to install locally.
 
 def stations_by_distance(stations: list[MonitoringStation], p: tuple[float, float]) -> list[tuple[MonitoringStation, float]]:
     """Returns a list of stations and their respective distances to the point at the coordinates of p.
     The returned list is sorted by distance, in ascending order."""
-    #Using insertion sort style, could be more efficient if a quick or merge sort were used.
+    #utils allows sort by key
     station_distance_list = []
     for station in stations:
         #Create tuple
         coord = station.get_coord()
         distance = distance_between_coords(coord, p)
-        station_distance_tuple = (station, distance)
-        #insert into list
-        flag = False #true when index of a distance in the list less than or equal to current distance is found
-        i = 0
-        while not flag and i < len(station_distance_list):
-            if distance <= station_distance_list[i][1]:
-                flag = True
-                station_distance_list.insert(i, station_distance_tuple)
-            else:
-                i+=1
+        station_distance_list.append((station, distance))
 
-        if flag == False:
-            station_distance_list.append(station_distance_tuple)
-        else:
-            raise Exception("Tuple was not inserted into the list, even though the flag was raised")
-
-        return station_distance_list
+    sorted_by_key(station_distance_list, 1)
+    return station_distance_list
 
 def distance_between_coords(p1: tuple[float, float], p2: tuple[float, float]) -> float:
-    """Returns the distance between the points given by the coords of p1 and p2"""
-    return sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1]) ** 2)
+    """Returns the distance between the points given by the coords of p1 and p2, using the Haversine formula from the haversine library.
+    Units of kilometres"""
+    return haversine(p1, p2)
--- test_geo.py
+++ test_geo.py
@@ -3,7 +3,9 @@ from floodsystem.geo import *
 """Unit test for the geo module"""
 
 def test_distance_between_coords():
-    p1 = (1,2)
-    p2 = (4.0, 6.0)
-    assert distance_between_coords(p1, p2) == 5
+    #using example from haversine docs
+    p1 = (45.7597, 4.8422)
+    p2 = (48.8567, 2.3508)
+    x =  distance_between_coords(p1, p2)
+    assert round(x, 5) == 392.21725 # 5 digits after decimal point
 
