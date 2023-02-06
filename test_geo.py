6ef2ebc4b1cef2b748f48f1b037856458e49174a
Josh Richmond <jtrichmond222@gmail.com>
2023-02-06 21:02:27 +0000

    Removed comments. Amended test_geo so it wouldn't fail on corrupt data

--- test_geo.py
+++ test_geo.py
@@ -19,17 +19,20 @@ def test_stations_by_distance():
     assert len(stations_distance_list) == len(stations)
     for i in range(len(stations) -1):
         assert stations_distance_list[i][0] in stations
+        assert distance_between_coords(stations_distance_list[i][0].coord, point) == stations_distance_list[i][1]
         assert stations_distance_list[i][1] <= stations_distance_list[i+1][1]
 
 def test_stations_within_range():
     stations = build_station_list()
-    point = (52.2053, 0.1218) # Cambridge, as known number of stations nearby
+    point = (52.2053, 0.1218) # Cambridge
     distance = 10 #km
     stations_near = stations_within_radius(stations, point, distance)
-    assert len(stations_near) == 11 # from representative output in 1C. If this test in particular fails, check the data was not corrupted.
     for station in stations_near:
         assert station in stations
         assert distance_between_coords(station.coord, point) <= 10
+    for station in stations:
+        if station not in stations_near:
+            assert distance_between_coords(station.coord, point) > 10
 
 def test_stations_by_river():
     stations = sample_stations()
