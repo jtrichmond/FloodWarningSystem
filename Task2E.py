7a2d8d8e95e391b804242fdf293866f722fccf54
Josh Richmond <jtrichmond222@gmail.com>
2023-02-20 22:30:55 +0000

    Wrote analysis code Wrote plot code Wrote 2E Started on 2F

--- Task2E.py
+++ Task2E.py
@@ -2,12 +2,18 @@ from floodsystem.plot import *
 from floodsystem.station import *
 from floodsystem.flood import stations_highest_rel_level
 from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.datafetcher import fetch_measure_levels
+from datetime import datetime, timedelta
 
 def run():
     stations = build_station_list()
     update_water_levels(stations)
     N = 5
-    plot_stations = stations_highest_rel_level(stations, 5)
+    dt = timedelta(days=10) # time difference of 10 days
+    plot_stations = stations_highest_rel_level(stations, N)
+    for station in plot_stations:
+        dates, levels = fetch_measure_levels(station.measure_id, dt)
+        plot_water_levels(station, dates, levels)
 
 
 if __name__ == "__main__":
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,14 +1,36 @@
 """Module contains functions for displaying data as graphs"""
 from .station import *
-from datetime import datetime, timedelta
+from datetime import datetime
 import matplotlib.pyplot as plt
+from numpy import ones
+from .analysis import *
 
 def plot_water_levels(station: MonitoringStation, dates: list[datetime], levels: list[float]):
-    plt.plot(dates, levels, 'bx--') # plots dates on x, levels on y, blue dashed line with crosses at data points
+    """Plot the water levels over time for a station, alongside its typical high and low range values"""
+    plt.plot(dates, levels, 'bx--', label="water level") # plots dates on x, levels on y, blue dashed line with crosses at data points
+    onearray = ones(len(dates))
+    if station.typical_range_consistent():
+        plt.plot(dates, onearray*station.typical_range[0], label="typical low level")
+        plt.plot(dates, onearray*station.typical_range[1], label="typical high level")
+    
+    plot_setup_and_display(station)
+
+
+def plot_water_level_with_fit(station: MonitoringStation, dates: list[datetime], levels: list[float], p: int):
+    """Plots the actual water level data for the station with the corresponding least-squares fit polynomial model of degree p."""
+    poly, shift = polyfit(dates, levels, p)
+    model_levels = convert_polynomial_to_level_data(dates, poly, shift)
+    plt.plot(dates, model_levels, label="least squares regression model")
+
+    plot_water_levels(station, dates, levels)
+
+def plot_setup_and_display(station: MonitoringStation):
+    """formats and displays plot"""
     plt.xlabel("Date")
     plt.ylabel("Water level / m")
     plt.title(station.name)
     plt.xticks(rotation=45)
+    plt.legend()
 
     plt.tight_layout() # Makes sure plot doesn't cut off date labels
     plt.show()
